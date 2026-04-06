6ac70db0397ed5b56c722891b2139c16177606d7
neo1202 <neowuhuayo@gmail.com>
2026-04-06 13:30:05 -0400

    bench: add placeholder benchmark executables

---
+++ bench/bench_blocksize.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "bench_blocksize: not yet implemented\n";
+    return 0;
+}
---
+++ bench/bench_scaling.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "bench_scaling: not yet implemented\n";
+    return 0;
+}
